1b2b43c7cbdc405b362366dbef2552512079225a
Benjamin Okkema <ben@okkema.org>
2026-03-18 21:46:29 -0500

    feat: initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,54 @@
+module "page" {
+  source     = "app.terraform.io/okkema/page/cloudflare"
+  version    = "~> 1.0"
+  depends_on = [module.application, module.bucket]
+
+  account_id = var.cloudflare_account_id
+  zone_id    = var.cloudflare_zone_id
+  name       = var.github_repository
+  repo_name  = var.github_repository
+  hostnames  = [var.github_repository]
+  buckets    = [{ name = "BUCKET", bucket_name = module.bucket.id }]
+  secrets = [
+    { name = "CLOUDFLARE_ACCESS_DOMAIN", text = module.application.domain, },
+    { name = "CLOUDFLARE_ACCESS_AUD", text = module.application.aud, },
+    { name = "SENTRY_DSN", text = module.sentry.dsn, }
+  ]
+}
+
+module "application" {
+  source     = "app.terraform.io/okkema/application/cloudflare"
+  version    = "~> 1.1"
+  depends_on = [module.role]
+
+  account_id = var.cloudflare_account_id
+  zone_id    = var.cloudflare_zone_id
+  name       = var.github_repository
+  role       = var.auth0_role
+  use_root   = true
+  path       = "edit"
+}
+
+module "role" {
+  source  = "app.terraform.io/okkema/role/auth0"
+  version = "~> 0.1"
+
+  name = var.auth0_role
+}
+
+module "bucket" {
+  source  = "app.terraform.io/okkema/bucket/cloudflare"
+  version = "~> 2.0"
+
+  account_id = var.cloudflare_account_id
+  name       = var.github_repository
+}
+
+
+module "sentry" {
+  source  = "app.terraform.io/okkema/project/sentry"
+  version = "~> 0.4"
+
+  github_repository   = var.github_repository
+  platform            = "other"
+}
